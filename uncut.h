f490417a4a0b97fcb6b245e3c30da123552b98a9
Andre Renaud <arenaud@designa-electronics.com>
2018-07-24 15:34:30 +1200

    uncut.h: Add description of test function

--- uncut.h
+++ uncut.h
@@ -8,6 +8,10 @@
 #ifndef UNCUT_H
 #define UNCUT_H
 
+/**
+ * Defines a function to call to run a test.
+ * A return value of < 0 is considered a failure, and >= 0 is a success
+ */
 typedef int (*uncut_function)(void);
 
 /**
